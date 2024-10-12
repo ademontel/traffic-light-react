cc4647b229b55f756e5b2b40b0f2deaf8ed003ae
ademontel <53986476+ademontel@users.noreply.github.com>
2024-10-12 00:23:41 +0000

    se agregó el primer challenge, falta mejorar el formato

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -1,21 +1,41 @@
 import React, {useState} from "react";
 
 const TrafficLight = () => {
-    const [color, setColor] = useState("red");
+    const [colorSelected, setColorSelected] = useState("red");
+    const [colorList, setColorList] = useState(['red','yellow','green']);
+    const generateColor = () => {
+        const hexColors = "0123456789ABCDEF"
+        let color = '#';
+
+        for (let i = 0; i < 6; i++){
+            color = color + hexColors[Math.floor(Math.random()*hexColors.length)];
+            console.log(colorList);
+        }
+        return color;
+    }
+
     return (
         <div className="d-flex flex-column align-items-center">
             <div className="bg-dark" style={{width:"30px", height:"200px"}}></div>
             <div className="bg-dark rounded-5 justify-content-evenly flex-column d-flex align-items-center" style={{width:"200px", height:"600px"}}>
-                <div onClick={() => {
-                    setColor("red")
-                }} className={"bg-danger rounded-circle " + (color=="red" ? "onLightRed":"")} style={{width:"150px", height:"150px"}}></div>
-                <div onClick={() => {
-                    setColor("yellow")
-                }} className={"bg-warning rounded-circle " + (color=="yellow" ? "onLightYellow":"")} style={{width:"150px", height:"150px"}}></div>
-                <div onClick={() =>{
-                    setColor("green")
-                }} className={"bg-success rounded-circle " + (color=="green" ? "onLightGreen":"")} style={{width:"150px", height:"150px"}}></div>
+                {
+                    colorList.map( (color, indexColor) => {
+                        return (
+                            <div key={indexColor} onClick={() => {
+                                setColorSelected(color)
+                                }} 
+                                className={"rounded-circle " + (colorSelected==color ? "onLight":"")} 
+                                style={{width:"150px", height:"150px", backgroundColor: color}}></div>
+                        );
+                    })
+                }
             </div>
+            <button className="btn btn-success mt-3" onClick={() => {
+                if (colorList.length < 4) {
+                    setColorList([...colorList, generateColor() ])
+                }
+            }}> Generate
+            </button>
         </div>
     );
 } 
